c3a62475c354adece4fd59afefa5ff4b249d4e6f
Eskils <eskilsviggum@gmail.com>
2023-02-19 09:47:34 +0100

    Initial Commit

---
+++ SubdivisionSaver/SubdivisionSaverView.h
@@ -0,0 +1,12 @@
+//
+//  SubdivisionSaverView.h
+//  SubdivisionSaver
+//
+//  Created by Eskil Sviggum on 19/02/2023.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface SubdivisionSaverView : ScreenSaverView
+
+@end
